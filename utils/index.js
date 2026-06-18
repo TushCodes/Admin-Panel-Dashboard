04ec735726717261f4a638d668361b8c137d2ef5
TushCodes <42437750+TushCodes@users.noreply.github.com>
2026-06-18 13:16:02 +0000

    fix

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,6 @@
+// barrel file to export all utility functions and classes
+// UNUSED as of now
+
 export * from './apiResponse.js';
 export * from './asyncHandler.js';
 export * from './dbError.js';
